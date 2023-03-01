d8be4e1e0ffc636c2eee6e7e54f35740b4ec4e9f
tsheeley17 <tylersheeley@gmail.com>
2023-03-01 11:33:47 -0500

    inital commit

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+// import { robots } from './robots';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import '../containers/App.css';
+
+class App extends Component {
+    constructor () {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => this.setState({ robots: users}))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        console.log(event.target.value);
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
+
+    }
+    
+}
+
+export default App;
